a7f6f17debc7e6ff1bbb345278fe8887990112e8
Sh4d0w <Patrick-DE@users.noreply.github.com>
2019-04-15 17:16:37 +0200

    -Removed json debugg

--- frontend/src/components/NewMessage.vue
+++ frontend/src/components/NewMessage.vue
@@ -52,9 +52,6 @@
             <b-button type="submit" variant="primary">Submit</b-button>
             <b-button type="reset" variant="danger">Reset</b-button>
             </b-form>
-            <b-card class="mt-3" header="Form Data Result">
-            <pre class="m-0">{{ form }}</pre>
-            </b-card>
         </div>
         <Alert ref="yourMomGayAlert" v-bind:duration=3></Alert>
     </div>
